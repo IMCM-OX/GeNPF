15e26e4ca8ee009e8d4d60784416ff7b1926aac7
cmaronga <marongachristopher@gmail.com>
2023-08-08 15:58:05 +0100

    implement forloop for ADNI_unzip

--- R/ADNI_unzip.R
+++ R/ADNI_unzip.R
@@ -8,8 +8,6 @@
 #' @examples
 ADNI_unzip <- function(dest_folder = "data_unziped"){
 
-  library(foreach) # because of the %do%, need to find a better way of reffering
-
   # 'data' should not be used as folder name
   if(dest_folder %in% c("Data", "data")) stop("Choose a different destination folder name!")
 
@@ -20,11 +18,16 @@ ADNI_unzip <- function(dest_folder = "data_unziped"){
   # list capture folders within ADNI
   v_adnizip <- list.files(xfun::relative_path(here::here("data", "ADNI")))
 
-  # decompress round 1
-  foreach::foreach(adni_folder = v_adnizip)%do%{
+  # # decompress round 1
+  # foreach::foreach(adni_folder = v_adnizip)%do%{
+  #
+  #   # decompress to 'data' folder
+  #   system(command = str_glue("tar -xvf data/ADNI/{adni_folder} -C {dest_folder}/"))
+  # }
 
-    # decompress to 'data' folder
-    system(command = str_glue("tar -xvf data/ADNI/{adni_folder} -C {dest_folder}/"))
+  for (adni_folder in seq_along(v_adnizip)){
+      # decompress to 'data' folder
+      system(command = stringr::str_glue("tar -xvf data/ADNI/{v_adnizip[adni_folder]} -C {dest_folder}/"))
   }
 
   # decompress round 2
@@ -35,12 +38,27 @@ ADNI_unzip <- function(dest_folder = "data_unziped"){
   v_biospec <- list.files(xfun::relative_path(here::here({dest_folder})),
                           recursive = T, pattern = ".zst")
 
-  foreach::foreach(biospec_file = v_biospec)%do%{
+  # foreach::foreach(biospec_file = v_biospec)%do%{
+  #
+  #   # extract folder tag for path specification
+  #   path_folder <- str_split(string = biospec_file, pattern = "/")[[1]][1]
+  #
+  #   # decompress to 'data' folder
+  #   system(command = str_glue("tar -xvf {dest_folder}/{biospec_file} -C {dest_folder}/{path_folder}"))
+  # }
 
+  for (biospec_file in seq_along(v_biospec)){
     # extract folder tag for path specification
-    path_folder <- str_split(string = biospec_file, pattern = "/")[[1]][1]
+    path_folder <- stringr::str_split(string = v_biospec[biospec_file], pattern = "/")[[1]][1]
 
     # decompress to 'data' folder
-    system(command = str_glue("tar -xvf {dest_folder}/{biospec_file} -C {dest_folder}/{path_folder}"))
+    system(command = stringr::str_glue("tar -xvf {dest_folder}/{v_biospec[biospec_file]} -C {dest_folder}/{path_folder}"))
   }
+
+
 }
+
+
+
+
+
