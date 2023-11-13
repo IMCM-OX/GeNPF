4238e9c276a06a8062a003ccf397fdad5d80e229
cmaronga <marongachristopher@gmail.com>
2023-11-13 15:10:38 +0000

    unzip_contents function

---
+++ R/unzip_contents.R
@@ -0,0 +1,16 @@
+#' Unzip content of a file or folder containing zipped files
+#'
+#' @param file A link to Google bucket folder or file to be unzipped
+#' @param dest_folder destination folder for unzipped contents
+#' @return A folder with contents
+#' @export
+#'
+#' @examples
+unzip_contents <- function(file = NULL, dest_folder = "unzipped_files"){
+
+
+}
+
+
+
+
