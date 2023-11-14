e71b091d668cddbdd2685e7a69099317a93ec274
cmaronga <marongachristopher@gmail.com>
2023-11-14 13:48:54 +0000

    implement copy command, unzip function

--- R/unzip_contents.R
+++ R/unzip_contents.R
@@ -8,8 +8,18 @@
 #' @examples
 unzip_contents <- function(file = NULL, dest_folder = "unzipped_files"){
 
+  #  create destination folder in the current working directory
+  if (!file.exists(xfun::relative_path(here::here({{dest_folder}})))) {
+    dir.create(xfun::relative_path(here::here({{dest_folder}})), recursive = TRUE)}
 
+  # step 2: copy the file/folder into the destination folder
+  system(command = stringr::str_glue("gsutil -m cp gs://{file}/* {dest_folder}/"))
+
+  # step 3: unzip all the files in the folder
 }
+
+
+
 
 
 
